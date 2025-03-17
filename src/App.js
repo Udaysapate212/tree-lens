7d1db179da3a581465b404be5f07f33959967ef5
Uday Sapate <udaysapate212@gmail.com>
2025-03-17 15:04:54 +0530

    Init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, {useEffect, useState} from 'react';
+
+const App=() =>{
+
+  return(
+    <div>
+      <h1>Hey!!!</h1>
+      <h2>How are you !</h2>
+      <h2>-Uday Sapate</h2>
+    </div>
+  );
+}
+
+export default App;
